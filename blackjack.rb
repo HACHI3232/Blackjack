d69c718cb7947a26e3e8540443bb2f2958f7486b
HACHI3232 <harada.chisato526@gmail.com>
2023-10-29 01:39:02 +0900

    Rubocupでのコードチェック

--- blackjack.rb
+++ blackjack.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Hand
   def initialize
     @cards = []
@@ -16,13 +18,14 @@ class Hand
       aces += 1 if card[:number] == 1
     end
 
-    while aces > 0 && total + 10 <= 21
+    while aces.positive? && total + 10 <= 21
       total += 10
       aces -= 1
     end
 
     total
   end
+
   def current_cards
     @cards
   end
@@ -34,12 +37,10 @@ class BlackJack
     @dealer = Hand.new
   end
 
-  def start 
+  def start
     deal_first_card_to_both
     deal_card_to_player
-    if @player.calculate_total <= 21
-      deal_card_to_dealer
-    end
+    deal_card_to_dealer if @player.calculate_total <= 21
     compare
   end
 
@@ -49,20 +50,18 @@ class BlackJack
     p "あなたの得点は#{player_score}です"
     p "ディーラーの得点は#{dealer_score}です"
     if player_score > 21
-      p "バーストしました！あなたの負けです"
+      p 'バーストしました！あなたの負けです'
     elsif dealer_score > 21 || player_score > dealer_score
-      p "あなたの勝ちです"
+      p 'あなたの勝ちです'
     else
-      p "ディーラーの勝ちです"
+      p 'ディーラーの勝ちです'
     end
   end
 
   def adjust_ace_value(score, cards)
     aces = cards.select { |card| card[:number] == 1 }
     aces.each do
-      if score + 10 <= 21
-        score += 10
-      end
+      score += 10 if score + 10 <= 21
     end
     score
   end
@@ -74,14 +73,14 @@ class BlackJack
       total += card[:number]
       aces = 1 if card[:number] == 1
     end
-    while aces > 0 && total <= 21
+    while aces.positive? && total <= 21
       total += 10
       aces -= 1
     end
     total
   end
 
-  def deal_first_card_to_both 
+  def deal_first_card_to_both
     @player.add_card(Card.random_card)
     @player.add_card(Card.random_card)
     @dealer.add_card(Card.random_card)
@@ -93,44 +92,41 @@ class BlackJack
     p "ディーラーの引いたカードは#{dealer_cards[0][:suit]}の#{dealer_cards[0][:number]}ともう一枚のカードはわかりません"
   end
 
-
   def deal_card_to_player
-    while 21 > @player.calculate_total
+    while @player.calculate_total < 21
       p "あなたの現在の得点は#{@player.calculate_total}です。カードを引きますか？(y/n)"
-      user_input = gets.chomp 
-      if user_input.downcase == "y"
+      user_input = gets.chomp
+      if user_input.downcase == 'y'
         @player.add_card(Card.random_card)
         p "あなたの引いたカードは#{@player.current_cards.last[:suit]}の#{@player.current_cards.last[:number]}です."
-      elsif user_input.downcase == "n"
+      elsif user_input.downcase == 'n'
         break
       else
-        p "無効な入力です。カードを引きますか？(y/n)"
+        p '無効な入力です。カードを引きますか？(y/n)'
       end
     end
   end
 
   def deal_card_to_dealer
-    while @dealer.calculate_total < 17
-      @dealer.add_card(Card.random_card)
-    end
+    @dealer.add_card(Card.random_card) while @dealer.calculate_total < 17
   end
 
   class Card
-    def self.random_card 
-      suits = ["ハート","ダイヤ","スペード","クラブ"]
-      numbers = [2,3,4,5,6,7,8,9,"ジャック","クイーン","キング","A"]
+    def self.random_card
+      suits = %w[ハート ダイヤ スペード クラブ]
+      numbers = [2, 3, 4, 5, 6, 7, 8, 9, 'ジャック', 'クイーン', 'キング', 'A']
       suit = suits.sample
       number = numbers.sample
 
       number = case number
-        when "ジャック","クイーン","キング" 
-          number = 10
-        when "A"
-          [1,11].sample
-        else number.to_i
-        end
+               when 'ジャック', 'クイーン', 'キング'
+                 number = 10
+               when 'A'
+                 [1, 11].sample
+               else number.to_i
+               end
 
-      {suit: suit, number: number}
+      { suit: suit, number: number }
     end
   end
 end
